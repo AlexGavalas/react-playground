5378d0c3f580ab1133c204e5c26253aa4471f9a2
AlexGavalas <alex.gavalas.92@gmail.com>
2021-11-08 17:23:26 +0200

    test

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ const double = (source) =>
     });
 
 of(1, 2, 3).pipe(double).subscribe(console.log);
+
+const obs = new Observable((subscriber) => {
+    subscriber.next(10);
+});
+
+const observer = {
+    next: console.log,
+    error: console.log,
+    complete: console.log,
+};
+
+obs.subscribe(observer);
