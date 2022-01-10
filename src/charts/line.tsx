cfc36fdaf8e28367b1cf62b1fe926c9989d482d8
AlexGavalas <alex.gavalas.92@gmail.com>
2022-01-10 20:13:11 +0200

    feat: dynamic chart title

--- src/charts/line.tsx
+++ src/charts/line.tsx
@@ -12,13 +12,15 @@ const MARGIN = {
     top: 10,
     right: 10,
     bottom: 10,
-    left: 10,
+    left: 20,
 };
 
 export const LineChart = () => {
     const container = useRef<HTMLDivElement>(null);
     const wrapper = useRef<HTMLDivElement>(null);
+    const titleRef = useRef<HTMLHeadingElement>(null);
     const [data, setData] = useState<Data[]>([]);
+    const [title, setTitle] = useState('A title');
 
     const { height = 0, width = 0 } = useElementSize(wrapper);
 
@@ -36,8 +38,12 @@ export const LineChart = () => {
         const xMinVal = d3.min(data, (d) => d.label) ?? 0;
         const xMaxVal = d3.max(data, (d) => d.label) ?? 0;
 
+        if (!titleRef.current) return;
+
         const INNER_WIDTH = width - MARGIN.left - MARGIN.right;
-        const INNER_HEIGHT = height - MARGIN.top - MARGIN.bottom;
+
+        const INNER_HEIGHT =
+            height - MARGIN.top - MARGIN.bottom - titleRef.current.offsetHeight;
 
         const svg = d3
             .select(container.current)
@@ -64,13 +70,21 @@ export const LineChart = () => {
             .attr('class', 'grid')
             .attr('transform', `translate(0, ${INNER_HEIGHT})`)
             .attr('opacity', 0.2)
-            .call(d3.axisBottom(xScale).tickSize(-INNER_HEIGHT));
+            .call(
+                d3.axisBottom(xScale).tickSize(-INNER_HEIGHT).tickFormat(null),
+            )
+            // Remove these labels
+            .selectAll('text')
+            .remove();
 
         // y-axis grid
         svg.append('g')
             .attr('class', 'grid')
             .attr('opacity', 0.2)
-            .call(d3.axisLeft(yScale).tickSize(-INNER_WIDTH));
+            .call(d3.axisLeft(yScale).tickSize(-INNER_WIDTH))
+            // Remove these labels
+            .selectAll('text')
+            .remove();
 
         // x-axis
         svg.append('g')
@@ -92,7 +106,7 @@ export const LineChart = () => {
         svg.append('path')
             .datum(data)
             .attr('fill', 'none')
-            .attr('stroke', '#d33')
+            .attr('stroke', 'aqua')
             .attr('stroke-width', 3)
             .attr('class', 'line')
             .attr('d', line);
@@ -110,11 +124,36 @@ export const LineChart = () => {
                 container.current.innerHTML = '';
             }
         };
-    }, [data, width, height]);
+    }, [data, width, height, title]);
 
     return (
-        <div style={{ height: '100%' }}>
-            <h4>Line chart with d3</h4>
+        <div
+            style={{
+                height: '100%',
+                display: 'grid',
+                gridTemplateRows: 'auto auto 1fr',
+            }}
+        >
+            <h4
+                style={{
+                    wordBreak: 'break-all',
+                    width: '300px',
+                }}
+                ref={titleRef}
+            >
+                {title}
+            </h4>
+            <button
+                onClick={() =>
+                    setTitle(
+                        'A very long title to show responsive height'.repeat(
+                            10,
+                        ),
+                    )
+                }
+            >
+                Change title
+            </button>
             <div style={{ height: '100%' }} ref={wrapper}>
                 <div style={{ height: '100%' }} ref={container} />
             </div>
